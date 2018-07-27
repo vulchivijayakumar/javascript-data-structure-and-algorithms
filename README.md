8646951c9ec7850b9ee0ab005dab83638d3ba73d
Vijaya Kumar Vulchi <vulchi.vijay@hotmail.com>
2018-07-27 22:35:39 +0530

    Queue added

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ Learning JavaScripit Data Structure and Algorithms
 
 Note: No definings included. Only practiced programms included in this repo for reference/re-call myself.
 
+Reference url: https://github.com/vijayvulchi/algoClass
+
 Index
 
 1. Classes
--- Stacks and Queues/stacks-and-queues.js
+++ Stacks and Queues/stacks-and-queues.js
@@ -42,13 +42,73 @@ myTodos.push('Pick up friend');
 myTodos.push('Take a breakfast');
 myTodos.pop(); // remove last string i.e 'Take a breakfast';
 myTodos.pop(); // remove last string i.e 'Pick up friend';
-myTodos.size();
+myTodos.size(); // size of the string includes $$$
 
-
-
-
-
-
+// Practice: Instead of string use object. then  array if you have time.
+ 
 /*
   Queues - FirstIn and FirstOut (FIFO) - Array
+  first item enqueued - added into the queue
+  first item dequeued - taken out of the queue
+  1. Storage // Constructor Function
+  2. enqueue() // Methods
+  3. dequeue()
+  4. Size()
+  We are using object now
 */
+
+function Queue(capacity) {
+  this.capaticy = capacity || Infinity;
+  this.storage = {};
+  this.head = 0; // starting point
+  this.tail = 0; // ending point
+}
+
+Queue.prototype.enqueue = function (value) {
+  if (this.count() < this.capaticy) {
+    this.storage[this.tail++] = value;
+    return this.count();
+  }
+  return 'Max capacity already reached. Remove element before adding a new one.';
+}
+
+Queue.prototype.dequeue = function () {
+  let ele = this.storage[this.head];
+  delete this.storage[this.head];
+  if (this.head < this.tail) this.head++;
+  return ele;
+}
+
+Queue.prototype.peek = function () { // first element
+  return this.storage[this.head];
+}
+
+Queue.prototype.count = function () {
+  return this.tail - this.head;
+}
+
+Queue.prototype.contains = function (value) {
+  for (let i = this.head; i < this.tail; i++) {
+    if (this.storage[i] === value) return true;
+  }
+  return false;
+}
+
+Queue.prototype.until = function (value) {
+  for (let i = this.head; i < this.tail; i++) {
+    if (this.storage[i] === value) return i - this.head + 1;
+  }
+  return null;
+}
+
+let myQueue = new Queue(5);
+myQueue.enqueue('vijay');
+myQueue.enqueue(1);
+myQueue.enqueue('kumar');
+
+myQueue.peek(); // vijay
+myQueue.count(); // 3
+myQueue.contains('vijay'); // true
+myQueue.until(1); // location 2
+myQueue.dequeue(); // removes peek element that is vijay.
+
