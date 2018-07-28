46f6e3e5b1410247bd0b4a819760092f7f0f8a2b
Vijaya Kumar Vulchi <vulchi.vijay@hotmail.com>
2018-07-28 16:35:34 +0530

    Big-O

---
+++ Big O/bigo.js
@@ -0,0 +1,38 @@
+/*
+ * Data Structures
+ * All about Big O
+ * 1. Space vs Time complexity
+ * 2. Calculating time complexity
+ * 3. Understanding Big-O
+ * 4. Calculating Big-O of js Operations
+ * 5. Calculating Big-O of loops 
+ * 6. Calculating time complexity.
+*/
+
+/*
+ * Space complexity
+ * 1. How much memory used?
+ * Time complexity
+ * 1. How many comparisions are made?
+ * 2. How many swaps are made?
+*/
+
+/*
+ * Time complexity examples:
+ * shift complexity - add elemement in array[0], move existing elements to next element location.
+ * unshift complexity - remove element in array[0], move existing elements to prev element location.
+*/
+
+//
+let counterChars = function (str) {
+  let count = 0;
+  for (count; count < str.length; count++) {
+    console.log(count);
+  }
+}
+
+counterChars('vijaya');
+counterChars('kumar vulchi');
+
+
+
--- README.md
+++ README.md
@@ -12,6 +12,13 @@ Content:
 4. Recursion
 5. Time Complexity - Big O
 6. Elementary Sorting
+7. Sorting Algorithms
+8. Tree & Searching
+9. Reviewing Core Concepts
+10. Binary Trees
+11. Graphs & Paths
+12. Depth & Breadth-First Search
+13. Hash Tables
 
 
 Reference url: https://github.com/vijayvulchi/algoClass
