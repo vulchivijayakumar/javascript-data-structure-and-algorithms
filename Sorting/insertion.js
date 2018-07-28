268b7205ebd08cfd30d4009dffa03519bff01030
Vijaya Kumar Vulchi <vulchi.vijay@hotmail.com>
2018-07-28 22:06:57 +0530

    selection and insertion sorting

--- Sorting/insertion.js
+++ Sorting/insertion.js
@@ -3,3 +3,56 @@
  * All about bubble sorting.
  * 1. Insertion Sort
 */
+
+/*
+ * Selects the first element in an array and pushes it into a new array. As
+ * each new element is added , insert the new element in the correct order
+ * Way 1: without creating new array
+ * [9, 6, 8, 2, 5, 1] - original array
+ * [9, 6, 8, 2, 5, 1] - swap 9, 6
+ * [6, 9, 8, 2, 5, 1] - swap 9, 8
+ * [6, 8, 9, 2, 5, 1] - swap 2, 9
+ * [6, 8, 2, 9, 5, 1] - swap 8, 2
+ * [6, 2, 8, 9, 5, 1] - swap 6, 2
+ * [2, 6, 8, 9, 5, 1]
+ * [2, 6, 8, 5, 9, 1]
+ * [2, 6, 5, 8, 9, 1]
+ * [2, 5, 6, 8, 9, 1]
+ * [2, 5, 6, 8, 1, 9]
+ * [2, 5, 6, 1, 8, 9]
+ * [2, 5, 1, 6, 8, 9]
+ * [2, 1, 5, 6, 8, 9]
+ * [1, 2, 5, 6, 8, 9] - result array
+*/
+
+var insertionSort = function(array, comparator) {
+  comparator = comparator || defaultComparator;
+
+  // start at index 1 as sublist of array[0] is already sorted
+  for (var index=1; index<array.length; index++) {
+    var value = array[index];
+    var compareIndex = index-1;
+    // swap with left element while < value
+    while (compareIndex > -1 && comparator(array[compareIndex], value) > 0) {
+      array = swap(array, compareIndex, index);
+      index = compareIndex;
+      compareIndex--;
+    }
+  }
+  return array;
+};
+
+function defaultComparator(a,b) {
+  if (a < b) return -1; // a comes first
+  else if (a > b) return 1; // b comes first
+  return 0;
+};
+
+function swap (arr, i1, i2) {
+  var temp = arr[i1];
+  arr[i1] = arr[i2];
+  arr[i2] = temp;
+  return arr;
+}
+
+insertionSort([9, 6, 8, 2, 5, 1]);
--- Sorting/selection.js
+++ Sorting/selection.js
@@ -5,7 +5,57 @@
 */
 
 /*
+ * Selects the highest element in an array and pushes it into a new array
+ * Way 1:
+ * original array [4, 8, 3, 1]
+ * [4, 8, 3, 1] find highest one. swap highest number and array last element
+ * [4, 1, 3, 8] find next highest one. swap highest number with array last element - 1
+ * [3, 1, 4, 8] find next hightest one. swap highest number width array last elemet - 2
+ * [1, 3, 4, 8] result array
  * Selects the smallest element in an array and pushes it into a new array
- * [1, 6, 8, 2, 5]
- * [1, 2, 5, 6, 8]
+ * Way 2: without creating new array
+ * original array [4, 6, 9, 3, 1, 5]
+ * [4, 6, 9, 3, 1, 5] - find smallest one and replace it in array[0]
+ * [1, 6, 9, 3, 4, 5] - find next smallest one and replace it in array[1]
+ * [1, 3, 9, 6, 4, 5] - find next smallest one and replace it in array[2]
+ * [1, 3, 4, 6, 9, 5] - find next smallest one and replace it in array[3]
+ * [1, 3, 4, 5, 9, 6] - find next smallest one and replace it in array[4]
+ * [1, 3, 4, 5, 6, 9] - result
 */
+
+// finding smallest one
+
+var selectionSort = function (array, comparator) {
+  comparator = comparator || defaultComparator;
+  array.forEach(function(element, index) {
+    var minValue = element;
+    var minIndex = index;
+    for (var i = index; i<array.length; i++) {
+      if (comparator(array[i], minValue) < 0)
+        {
+          minValue = array[i];
+          minIndex = i;
+        }
+    }
+    // swap values at min index and current position
+    array = swap(array, index, minIndex);
+  });
+  return array;
+};
+
+function swap (arr, i1, i2) {
+  var temp = arr[i1];
+  arr[i1] = arr[i2];
+  arr[i2] = temp;
+  return arr;
+}
+
+function defaultComparator(a,b) {
+  if (a < b) return -1;
+  else if (a > b) return 1;
+  return 0;
+};
+
+selectionSort([4, 6, 9, 3, 1, 5]);
+
+// try highest one
